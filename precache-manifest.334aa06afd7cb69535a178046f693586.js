4a228cbaa5f022fdf0faf37da601f1660e2d23af
dania_abdul <43537934+Dania-abdul@users.noreply.github.com>
2020-05-16 15:06:18 +0200

    Updates

---
+++ precache-manifest.334aa06afd7cb69535a178046f693586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce761bb696d7c5fe0434a300bc819a4",
+    "url": "/fluent-uI--playground/index.html"
+  },
+  {
+    "revision": "dcc9820de83aa3040c86",
+    "url": "/fluent-uI--playground/static/css/main.c3d8ea79.chunk.css"
+  },
+  {
+    "revision": "34dbb5867cd3143bb8c9",
+    "url": "/fluent-uI--playground/static/js/2.4031f763.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/fluent-uI--playground/static/js/2.4031f763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc9820de83aa3040c86",
+    "url": "/fluent-uI--playground/static/js/main.5700dfab.chunk.js"
+  },
+  {
+    "revision": "b423d71a2daa31acb5c5",
+    "url": "/fluent-uI--playground/static/js/runtime-main.d3f9fab5.js"
+  }
+]);
